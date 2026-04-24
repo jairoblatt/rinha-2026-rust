5f3424f4351bca6aeae09b29c6416bd617653bdf
jairoblatt <jairoblattc@gmail.com>
2026-04-24 18:15:39 -0300

    revert

--- src/data.rs
+++ src/data.rs
@@ -1,42 +1,23 @@
 use std::sync::OnceLock;
 
 pub const N: usize = 100_000;
-
-#[repr(C, align(16))]
-#[derive(Copy, Clone)]
-pub struct PackedRef {
-    pub cont: [i16; 6],
-    pub packed: [u8; 3],
-    pub _pad: u8,
-}
-
-const _: () = assert!(std::mem::size_of::<PackedRef>() == 16);
-const _: () = assert!(std::mem::align_of::<PackedRef>() == 16);
+pub const STRIDE: usize = 16;
 
 #[repr(C, align(64))]
-pub struct AlignedRefs(pub [PackedRef; N]);
+pub struct AlignedRefs(pub [i16; N * STRIDE]);
 
 static REFS: OnceLock<Box<AlignedRefs>> = OnceLock::new();
 static LABELS_BYTES: &[u8; N] = include_bytes!(concat!(env!("OUT_DIR"), "/labels.bin"));
-static PACKED_BYTES: &[u8; N * 16] =
-    include_bytes!(concat!(env!("OUT_DIR"), "/packed.bin"));
-
-include!(concat!(env!("OUT_DIR"), "/dict.rs"));
+static REFS_BYTES: &[u8; N * STRIDE * 2] = include_bytes!(concat!(env!("OUT_DIR"), "/refs.bin"));
 
 pub fn init() {
-    let mut boxed: Box<AlignedRefs> = Box::new(AlignedRefs(
-        [PackedRef {
-            cont: [0; 6],
-            packed: [0; 3],
-            _pad: 0,
-        }; N],
-    ));
+    let mut boxed: Box<AlignedRefs> = Box::new(AlignedRefs([0i16; N * STRIDE]));
 
     unsafe {
         std::ptr::copy_nonoverlapping(
-            PACKED_BYTES.as_ptr(),
+            REFS_BYTES.as_ptr(),
             boxed.0.as_mut_ptr() as *mut u8,
-            N * 16,
+            N * STRIDE * 2,
         );
     }
 
@@ -46,7 +27,7 @@ pub fn init() {
 }
 
 #[inline(always)]
-pub fn refs() -> &'static [PackedRef; N] {
+pub fn refs() -> &'static [i16; N * STRIDE] {
     &REFS.get().expect("call data::init() first").0
 }
 
--- src/knn.rs
+++ src/knn.rs
@@ -1,6 +1,4 @@
-use crate::data::{
-    self, PackedRef, DICT01, DICT03, DICT04, DICT08, DICT12,
-};
+use crate::data;
 use std::arch::x86_64::*;
 
 pub struct Top5 {
@@ -16,55 +14,11 @@ pub fn knn5(query: &[i16; 16]) -> Top5 {
     unsafe { knn5_avx2(query) }
 }
 
-#[inline(always)]
-fn sq_diff(a: i16, b: i16) -> u32 {
-    let d = a as i32 - b as i32;
-    (d * d) as u32
-}
-
 #[target_feature(enable = "avx2")]
 unsafe fn knn5_avx2(query: &[i16; 16]) -> Top5 {
-    let mut pd01 = [0u32; 16];
-    let mut pd03 = [0u32; 32];
-    let mut pd04 = [0u32; 8];
-    let mut pd08 = [0u32; 32];
-    let mut pd12 = [0u32; 16];
+    let q = _mm256_loadu_si256(query.as_ptr() as *const __m256i);
 
-    let q01 = query[1];
-    let q03 = query[3];
-    let q04 = query[4];
-    let q08 = query[8];
-    let q12 = query[12];
-    let q09 = query[9];
-    let q10 = query[10];
-    let q11 = query[11];
-
-    for k in 0..16 {
-        pd01[k] = sq_diff(q01, DICT01[k]);
-    }
-    for k in 0..32 {
-        pd03[k] = sq_diff(q03, DICT03[k]);
-    }
-    for k in 0..8 {
-        pd04[k] = sq_diff(q04, DICT04[k]);
-    }
-    for k in 0..32 {
-        pd08[k] = sq_diff(q08, DICT08[k]);
-    }
-    for k in 0..16 {
-        pd12[k] = sq_diff(q12, DICT12[k]);
-    }
-
-    let pd09: [u32; 2] = [sq_diff(q09, 0), sq_diff(q09, 8192)];
-    let pd10: [u32; 2] = [sq_diff(q10, 0), sq_diff(q10, 8192)];
-    let pd11: [u32; 2] = [sq_diff(q11, 0), sq_diff(q11, 8192)];
-
-    let q_cont_arr: [i16; 8] = [
-        query[0], query[2], query[5], query[6], query[7], query[13], 0, 0,
-    ];
-    let q_cont = _mm_loadu_si128(q_cont_arr.as_ptr() as *const __m128i);
-
-    let refs_ptr = data::refs().as_ptr() as *const __m128i;
+    let refs_ptr = data::refs().as_ptr() as *const __m256i;
 
     let mut td = [u32::MAX; 5];
     let mut ti = [0u32; 5];
@@ -73,38 +27,20 @@ unsafe fn knn5_avx2(query: &[i16; 16]) -> Top5 {
     let n = data::N;
     let mut i = 0usize;
     while i < n {
-        if i + 8 < n {
-            _mm_prefetch(refs_ptr.add(i + 8) as *const i8, _MM_HINT_T0);
+        if i & 1 == 0 && i + 4 < n {
+            _mm_prefetch(refs_ptr.add(i + 4) as *const i8, _MM_HINT_T0);
         }
 
-        let row = _mm_load_si128(refs_ptr.add(i));
+        let r = _mm256_load_si256(refs_ptr.add(i));
+        let diff = _mm256_sub_epi16(q, r);
+        let sq = _mm256_madd_epi16(diff, diff);
 
-        let ref_cont = _mm_insert_epi32(row, 0, 3);
-        let diff = _mm_sub_epi16(q_cont, ref_cont);
-        let sq = _mm_madd_epi16(diff, diff);
-        let s = _mm_add_epi32(sq, _mm_srli_si128(sq, 8));
+        let lo = _mm256_castsi256_si128(sq);
+        let hi = _mm256_extracti128_si256(sq, 1);
+        let s = _mm_add_epi32(lo, hi);
+        let s = _mm_add_epi32(s, _mm_srli_si128(s, 8));
         let s = _mm_add_epi32(s, _mm_srli_si128(s, 4));
-        let mut dist = _mm_cvtsi128_si32(s) as u32;
-
-        let packed24 = _mm_extract_epi32(row, 3) as u32;
-        let i01 = ( packed24       ) & 0x0F;
-        let i03 = ( packed24 >>  4 ) & 0x1F;
-        let i04 = ( packed24 >>  9 ) & 0x07;
-        let i08 = ( packed24 >> 12 ) & 0x1F;
-        let i12 = ( packed24 >> 17 ) & 0x0F;
-        let b09 = ( packed24 >> 21 ) & 0x01;
-        let b10 = ( packed24 >> 22 ) & 0x01;
-        let b11 = ( packed24 >> 23 ) & 0x01;
-
-        dist = dist
-            .wrapping_add(*pd01.get_unchecked(i01 as usize))
-            .wrapping_add(*pd03.get_unchecked(i03 as usize))
-            .wrapping_add(*pd04.get_unchecked(i04 as usize))
-            .wrapping_add(*pd08.get_unchecked(i08 as usize))
-            .wrapping_add(*pd12.get_unchecked(i12 as usize))
-            .wrapping_add(*pd09.get_unchecked(b09 as usize))
-            .wrapping_add(*pd10.get_unchecked(b10 as usize))
-            .wrapping_add(*pd11.get_unchecked(b11 as usize));
+        let dist = _mm_cvtsi128_si32(s) as u32;
 
         if dist < threshold {
             insert(&mut td, &mut ti, dist, i as u32);
@@ -148,29 +84,4 @@ pub fn fraud_count(idx: &[u32; 5]) -> u8 {
         + l[idx[2] as usize]
         + l[idx[3] as usize]
         + l[idx[4] as usize]
-}
-
-#[allow(dead_code)]
-#[inline]
-pub(crate) fn decode_i16(row: &PackedRef, dim: usize) -> i16 {
-    let u24 = (row.packed[0] as u32)
-        | ((row.packed[1] as u32) << 8)
-        | ((row.packed[2] as u32) << 16);
-    match dim {
-        0 => row.cont[0],
-        2 => row.cont[1],
-        5 => row.cont[2],
-        6 => row.cont[3],
-        7 => row.cont[4],
-        13 => row.cont[5],
-        1 => DICT01[((u24) & 0x0F) as usize],
-        3 => DICT03[((u24 >> 4) & 0x1F) as usize],
-        4 => DICT04[((u24 >> 9) & 0x07) as usize],
-        8 => DICT08[((u24 >> 12) & 0x1F) as usize],
-        12 => DICT12[((u24 >> 17) & 0x0F) as usize],
-        9 => if ((u24 >> 21) & 1) == 1 { 8192 } else { 0 },
-        10 => if ((u24 >> 22) & 1) == 1 { 8192 } else { 0 },
-        11 => if ((u24 >> 23) & 1) == 1 { 8192 } else { 0 },
-        _ => 0,
-    }
 }
